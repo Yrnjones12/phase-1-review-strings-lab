3cd7ebc9f66cd21985d42ed0e0a4be8b192ce329
38jones <Yrnjones12>
2023-07-16 23:15:04 +0300

    Completed assignment

---
+++ function.js
@@ -0,0 +1,19 @@
+//  Anatomy of a function
+
+// function funcName(p1){
+// block of code
+// }
+
+// funcName(arg1)
+
+function giveName(name){
+    console.log("this is",name)
+}
+
+// giveName()
+
+// A function that takes in 2 numbers and adds them together.
+
+// function giveName = (num1,num2){
+    //  let sum = num1 
+}
